8db3887db1cfd3ba2bd7a4fec22953459f9efbbf
Tony DiCola <tony@tonydicola.com>
2015-05-26 17:22:30 -0700

    Refactor elfquery to add sections table and improve schema.  Added to_hex and from_hex SQLite functions.

--- Adafruit_Legolas/commands/elfquery.py
+++ Adafruit_Legolas/commands/elfquery.py
@@ -9,26 +9,107 @@ import sys
 
 import click
 from elftools.common.py3compat import bytes2str
-from elftools.elf.dynamic import DynamicSection
 from elftools.elf.elffile import ELFFile
 from elftools.elf.descriptions import *
 from elftools.elf.sections import SymbolTableSection
-from elftools.elf.gnuversions import *
 from tabulate import tabulate
 
 from ..main import main
 
 
-# Define symbol column names and types.
-COLUMNS = [('Section', 'text not null'),
-           ('Number',  'integer not null'),
-           ('Value',   'text'),
-           ('Size',    'integer'),
-           ('Type',    'text'),
-           ('Bind',    'text'),
-           ('Vis',     'text'),
-           ('Ndx',     'text'),
-           ('Name',    'text')]
+# Symbol table column names and types.
+# TODO: Convert value to integer and convert to hex when printing.
+SYMBOL_COLS = [('Number',       'integer primary key autoincrement'),
+               ('Value',        'integer'),
+               ('Size',         'integer'),
+               ('Type',         'text'),
+               ('Binding',      'text'),
+               ('Visibility',   'text'),
+               ('SectionIndex', 'text'),
+               ('Name',         'text'),
+               ('Section',      'text')]
+
+# Section header table column names and types.
+SECTION_COLS = [('Number',    'integer primary key'),
+                ('Name',      'text'),
+                ('Type',      'text'),
+                ('Flags',     'text'),
+                ('Address',   'integer'),
+                ('Offset',    'integer'),
+                ('Size',      'integer'),
+                ('Link',      'integer'),
+                ('Info',      'integer'),
+                ('Alignment', 'integer'),
+                ('EntrySize', 'integer')]
+
+# Example queries that are shown in documentation:
+EXAMPLES = """The following are example queries:
+
+To list every symbol in order:
+
+  SELECT * FROM symbols ORDER BY Number ASC
+
+Note that a TO_HEX function is available which can convert a column to a 
+zero-padded hexadecimal string of the specified width for easier reading.
+For example to grab the name and value of each symbol:
+
+  SELECT Name, TO_HEX(Value, 8) FROM symbols ORDER BY Number ASC
+
+To select the name of each symbol related to the '.bss' section:
+
+  SELECT Name FROM symbols WHERE Section = '.bss'"
+
+To do the same query but also restrict it to symbols with size > 256 bytes:
+
+  SELECT Name FROM symbols WHERE Section = '.bss' AND Size > 256"
+
+There is also a FROM_HEX function that can be used to filter against hex values
+easily.  For example to select all symbols with value between 0xFF and 0xFFFF:
+
+  SELECT Name, TO_HEX(Value, 8) FROM symbols WHERE Value > FROM_HEX('FF') AND Value < FROM_HEX('FFFF')
+
+To select all symbol data for symbols that are of type 'FUNC' or 'OBJECT':
+
+  SELECT * FROM symbols WHERE Type IN ('FUNC', 'OBJECT')
+
+To select the name and size of the 5 largest symbols:
+
+  SELECT Name, Size FROM symbols ORDER BY Size DESC LIMIT 5
+
+To list all variables in RAM sorted by size:
+
+  SELECT Size, Name, Value FROM symbols WHERE Type = 'OBJECT' ORDER BY Size ASC
+
+To show the size of all values in the above RAM list:
+
+  SELECT SUM(Size) FROM symbols WHERE Type = 'OBJECT'
+
+You can even do more advanced queries like counting how many unique Type
+values exist:
+
+  SELECT Type, COUNT(*) AS Count FROM symbols GROUP BY Type ORDER BY Count DESC
+
+Any query supported by SQLite is possible!
+"""
+
+# Main help for the program (builds on the examples above).
+USAGE = """Query ELF symbols using a SQL-style query.
+
+Provide two arguments, a path to an ELF file and an optional SQL query to 
+make against the file.  The SQL query should be made against the table 
+'symbols' and it contains a row for each symbol.  In addition there is a 
+'sections' table that lists information about each section.  See the
+--list-columns option to list all the columns and tables.
+
+If no query is provided then an interactive command loop will start where 
+multiple queries can be run successively.
+
+Results will be written as a friendly table format to standard output by
+default.  However look at the --output option to write results to a file,
+and the --output-format option to write results in a machine-friendly format
+like a comma or tab separated file.  Note that the output and output format
+options are ignored in interactive query mode.
+"""
 
 
 class ELFQuery(object):
@@ -39,91 +120,45 @@ class ELFQuery(object):
     def __init__(self, input_file):
         # Parse ELF file and populate DB table with data.
         self.elffile = ELFFile(input_file)
-        self._init_versioninfo()
-        # Initialize and load DB with parsed data.
         self._init_db()
         self._load_db()
 
-    def _init_versioninfo(self):
-        # Initialize version info from ELF sections.  Adapted from readelf.py.
-        self._versioninfo = {'versym': None, 'verdef': None, 'verneed': None, 
-                             'type': None}
-
-        for section in self.elffile.iter_sections():
-            if isinstance(section, GNUVerSymSection):
-                self._versioninfo['versym'] = section
-            elif isinstance(section, GNUVerDefSection):
-                self._versioninfo['verdef'] = section
-            elif isinstance(section, GNUVerNeedSection):
-                self._versioninfo['verneed'] = section
-            elif isinstance(section, DynamicSection):
-                for tag in section.iter_tags():
-                    if tag['d_tag'] == 'DT_VERSYM':
-                        self._versioninfo['type'] = 'GNU'
-                        break
-
-        if not self._versioninfo['type'] and (self._versioninfo['verneed'] or \
-                                              self._versioninfo['verdef']):
-            self._versioninfo['type'] = 'Solaris'
-
-    def _symbol_version(self, nsym):
-        # Adapted from readelf.py.
-        symbol_version = dict.fromkeys(('index', 'name', 'filename', 'hidden'))
-
-        if not self._versioninfo['versym'] or nsym >= self._versioninfo['versym'].num_symbols():
-            return None
-
-        symbol = self._versioninfo['versym'].get_symbol(nsym)
-        index = symbol.entry['ndx']
-        if not index in ('VER_NDX_LOCAL', 'VER_NDX_GLOBAL'):
-            index = int(index)
-
-            if self._versioninfo['type'] == 'GNU':
-                # In GNU versioning mode, the highest bit is used to
-                # store wether the symbol is hidden or not
-                if index & 0x8000:
-                    index &= ~0x8000
-                    symbol_version['hidden'] = True
-
-            if self._versioninfo['verdef'] and index <= self._versioninfo['verdef'].num_versions():
-                _, verdaux_iter = self._versioninfo['verdef'].get_version(index)
-                symbol_version['name'] = bytes2str(next(verdaux_iter).name)
-            else:
-                verneed, vernaux = self._versioninfo['verneed'].get_version(index)
-                symbol_version['name'] = bytes2str(vernaux.name)
-                symbol_version['filename'] = bytes2str(verneed.name)
-
-        symbol_version['index'] = index
-        return symbol_version
-
-    def _format_hex(self, addr, fieldsize=None, fullhex=False, lead0x=True,
-                    alternate=False):
-        # Adapted from readelf.py.
-        if alternate:
-            if addr == 0:
-                lead0x = False
-            else:
-                lead0x = True
-                fieldsize -= 2
-
-        s = '0x' if lead0x else ''
-        if fullhex:
-            fieldsize = 8 if self.elffile.elfclass == 32 else 16
-        if fieldsize is None:
-            field = '%x'
-        else:
-            field = '%' + '0%sx' % fieldsize
-        return s + field % addr
-
     def _init_db(self):
-        # Initialize in memory SQLite DB to hold symbol data.
+        """Setup the in-memory database for symbol and section data."""
+        # Initialize in memory SQLite DB to hold ELF data.
         self.db = sqlite3.connect(':memory:')
+        # Add custom functions for hex conversion (although the latest SQLite
+        # versions support hex conversions natively, Mac OSX has a very old
+        # version of SQLite with Python and needs these functions).
+        self.db.create_function('to_hex', 2, to_hex)
+        self.db.create_function('from_hex', 1, from_hex)
+        # Create sections table.
         # Create column specification of form like "<name> <type>, <name> <type>, etc."
-        column_spec = ', '.join(map(lambda x: '{0} {1}'.format(x[0], x[1]), COLUMNS))
-        self.db.execute("CREATE TABLE symbols ({0}, PRIMARY KEY (Section, Number))".format(column_spec))
+        column_spec = ', '.join(map(lambda x: '{0} {1}'.format(x[0], x[1]), SECTION_COLS))
+        self.db.execute('CREATE TABLE sections ({0})'.format(column_spec))
+        # Create symbols table.
+        column_spec = ', '.join(map(lambda x: '{0} {1}'.format(x[0], x[1]), SYMBOL_COLS))
+        self.db.execute("CREATE TABLE symbols ({0})".format(column_spec))
         self.db.commit()
 
     def _load_db(self):
+        """Load symbol and section data into the database."""
+        # Load ELF section metadata.
+        for nsec, section in enumerate(self.elffile.iter_sections()):
+            self.db.execute('INSERT INTO sections VALUES (?,?,?,?,?,?,?,?,?,?,?)',
+                            (nsec,
+                             bytes2str(section.name).strip(),
+                             describe_sh_type(section['sh_type']).strip(),
+                             describe_sh_flags(section['sh_flags']).strip(),
+                             section['sh_addr'],
+                             section['sh_offset'],
+                             section['sh_size'],
+                             section['sh_link'],
+                             section['sh_info'],
+                             section['sh_addralign'],
+                             section['sh_entsize']))
+            self.db.commit()
+
         # Load ELF symbol data into DB.  Adapted from readelf.py.
         for section in self.elffile.iter_sections():
             if not isinstance(section, SymbolTableSection):
@@ -131,32 +166,28 @@ class ELFQuery(object):
             if section['sh_entsize'] == 0:
                 continue
             for nsym, symbol in enumerate(section.iter_symbols()):
-                version_info = ''
-                if (section['sh_type'] == 'SHT_DYNSYM' and self._versioninfo['type'] == 'GNU'):
-                    version = self._symbol_version(nsym)
-                    if (version['name'] != bytes2str(symbol.name) and
-                        version['index'] not in ('VER_NDX_LOCAL', 'VER_NDX_GLOBAL')):
-                        if version['filename']:
-                            # external symbol
-                            version_info = '@%(name)s (%(index)i)' % version
-                        else:
-                            # internal symbol
-                            if version['hidden']:
-                                version_info = '@%(name)s' % version
-                            else:
-                                version_info = '@@%(name)s' % version
-                # Add row to symbols table.
-                self.db.execute('INSERT INTO symbols VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
-                                (bytes2str(section.name), 
-                                 nsym,
-                                 self._format_hex(symbol['st_value'], fullhex=True, lead0x=False),
+                # Get the related section header index and name for this symbol.
+                shndx = describe_symbol_shndx(symbol['st_shndx']).strip()
+                related_section = None
+                if str.isdigit(shndx):
+                    sec = self._get_section(shndx)
+                    if sec is not None:
+                        related_section = sec[1]
+                # Write the symbol value to the table.
+                self.db.execute('INSERT INTO symbols VALUES (NULL,?,?,?,?,?,?,?,?)',
+                                (symbol['st_value'],
                                  symbol['st_size'],
-                                 describe_symbol_type(symbol['st_info']['type']),
-                                 describe_symbol_bind(symbol['st_info']['bind']),
-                                 describe_symbol_visibility(symbol['st_other']['visibility']),
-                                 describe_symbol_shndx(symbol['st_shndx']),
-                                 bytes2str(symbol.name)))
+                                 describe_symbol_type(symbol['st_info']['type']).strip(),
+                                 describe_symbol_bind(symbol['st_info']['bind']).strip(),
+                                 describe_symbol_visibility(symbol['st_other']['visibility']).strip(),
+                                 shndx,
+                                 bytes2str(symbol.name).strip(),
+                                 related_section))
                 self.db.commit()
+
+    def _get_section(self, index):
+        """Retrieve the specified section row at the provided index."""
+        return self.db.execute('SELECT * FROM sections WHERE Number = ?', (index,)).fetchone()
 
     def query(self, query):
         """Perform SQL query against symbols and return result rows and column
@@ -168,8 +199,8 @@ class ELFQuery(object):
 
 
 class InteractiveELFQuery(cmd.Cmd):
-    # Python Cmd module implementation for a simple interactive query loop.
-
+    """Python Cmd module implementation for a simple interactive query loop."""
+    # Change the prompt for the command loop.
     prompt = 'SQL> '
 
     def __init__(self, elfquery):
@@ -190,6 +221,10 @@ class InteractiveELFQuery(cmd.Cmd):
         """List the columns available to query."""
         print_columns()
 
+    def do_examples(self, line):
+        """Display example queries."""
+        click.echo(EXAMPLES)
+
     def default(self, query):
         """Run query against ELF file."""
         try:
@@ -199,16 +234,38 @@ class InteractiveELFQuery(cmd.Cmd):
             click.echo('ERROR: {0}'.format(ex.message))
 
 
+def to_hex(number, width):
+    """Convert number to hex value with specified width.  Will be padded by zero
+    to fill the width.
+    """
+    format_string = '{{0:0{0}X}}'.format(width)
+    return format_string.format(number)
+
+
+def from_hex(value):
+    """Convert hex string to an integer value."""
+    return int(value, 16)
+
+
 def print_columns():
-    # Print out the list of columns.
+    """Print out the list of columns."""
+    click.echo("Table 'sections' has the following columns:")
+    for col in SECTION_COLS:
+        click.echo('- {0}'.format(col[0]))
+    click.echo('Key to Flags column values:')
+    click.echo('W (write), A (alloc), X (execute), M (merge), S (strings), l (large)')
+    click.echo('I (info), L (link order), G (group), T (TLS), E (exclude), x (unknown)')
+    click.echo('O (extra OS processing required), o (OS specific), p (processor specific)')
+    click.echo('')
     click.echo("Table 'symbols' has the following columns:")
-    for col in COLUMNS:
-        click.echo('{0}'.format(col[0]))
+    for col in SYMBOL_COLS:
+        click.echo('- {0}'.format(col[0]))
 
 
 def list_columns(ctx, param, value):
-    # Called when the --list-columns option is parsed.  Print out columns and 
-    # stop any further processing.
+    """Called when the --list-columns option is parsed.  Print out columns and 
+    stop any further processing.
+    """
     if not value or ctx.resilient_parsing:
         return
     print_columns()
@@ -216,8 +273,9 @@ def list_columns(ctx, param, value):
 
 
 def print_results(result, columns, output, output_format):
-    # Print out the results of a query to the specified output and using the
-    # specified output format.
+    """Print out the results of a query to the specified output and using the
+    specified output format.
+    """
     if output_format == 'friendly':
         output.write(tabulate(result, columns))
         output.write('\n\n')
@@ -234,7 +292,7 @@ def print_results(result, columns, output, output_format):
         raise click.UsageError('Unknown output format!')
 
 
-@main.command(short_help='query symbols of an ELF file using a SQL query syntax')
+@main.command(help='{0}\n\n{1}'.format(USAGE, EXAMPLES))
 # Take an ELF file path to query as input.
 @click.argument('input_file',
                 metavar='FILE',
@@ -265,60 +323,8 @@ def print_results(result, columns, output, output_format):
               default=sys.stdout,
               help='result file (default is standard output)')
 # Define the command code.  Notice how click will send in parsed parameters as
-# arguments to the function.  Also note click will use the docstring as the
-# full help text for the command.
+# arguments to the function.
 def elfquery(input_file, query, output_format, output):
-    """Query ELF symbols using a SQL-style query.
-
-    Provide two arguments, a path to an ELF file and an optional SQL query to 
-    make against the file.  The SQL query should be made against the table 
-    'symbols' and it contains a row for each symbol (see the --list-columns
-    option to list the available column names).
-
-    If no query is provided then an interactive command loop will start where 
-    multiple queries can be run successively.
-
-    For example to list every symbol in order:
-
-      legolas elfquery <file> "SELECT * FROM symbols ORDER BY Section, Number ASC"
-
-    To select the name of each symbol from section '.symtab':
-
-      legolas elfquery <file> "SELECT Name FROM symbols WHERE Section = '.symtab'"
-
-    To do the same query but also restrict it to symbols with size > 256:
-
-      legolas elfquery <file> "SELECT Name FROM symbols WHERE Section = '.symtab' AND Size > 256"
-
-    To select all symbol data for symbols that are of type 'FUNC' or 'OBJECT':
-
-      legolas elfquery <file> "SELECT * FROM symbols WHERE Type IN ('FUNC', 'OBJECT')"
-
-    To select the name and size of the 5 largest symbols:
-
-      legolas elfquery <file> "SELECT Name, Size FROM symbols ORDER BY Size DESC LIMIT 5"
-
-    To list all variables in RAM sorted by size:
-
-      legolas elfquery <file> "SELECT Size, Name, Value FROM symbols WHERE Type = 'OBJECT' ORDER BY Size ASC"
-
-    To show the size of all values in the above RAM list:
-
-      legolas elfquery <file> "SELECT SUM(Size) FROM symbols WHERE Type = 'OBJECT'"
-
-    You can even do more advanced queries like counting how many unique Type
-    values exist:
-
-      legolas elfquery <file> "SELECT Type, COUNT(*) AS Count FROM symbols GROUP BY Type ORDER BY Count DESC"
-
-    Any query supported by SQLite is possible against the symbols table.
-
-    Results will be written as a friendly table format to standard output by
-    default.  However look at the --output option to write results to a file,
-    and the --output-format option to write results in a machine-friendly format
-    like a comma or tab separated file.  Note that the output and output format
-    options are ignored in interactive query mode.
-    """
     elfquery = ELFQuery(input_file)
     if query is not None:
         # Query was sent in command line, process it and then exit.
